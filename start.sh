7cfc7fac475e132ec979fbdb265cdabd9a7cbefb
DarkZeros <mailszeros@gmail.com>
2022-12-22 23:54:50 +0000

    Add service-templates

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Start script for gps_dbus
+#   First parameter: tty device to use
+#
+# Keep this script running with daemon tools. If it exits because the
+# connection crashes, or whatever, daemon tools will start a new one.
+#
+
+. /opt/victronenergy/serial-starter/run-service.sh
+
+app=/data/etc/dbus-vevor-inverter/dbus-vevor-inverter.py
+args="--dbus system -s /dev/$tty"
+
+# Baudrates to use
+start $args -b 2400
